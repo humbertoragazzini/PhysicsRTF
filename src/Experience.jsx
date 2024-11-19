d426b60a40884e3301594e50539d99f1a1e4087a
beto <beto_vicio@hotmail.com>
2024-11-19 21:18:26 +0000

    added object and label for the objects

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,4 +1,4 @@
-import { OrbitControls } from "@react-three/drei";
+import { Html, OrbitControls } from "@react-three/drei";
 import { Physics, RigidBody } from "@react-three/rapier";
 import { Perf } from "r3f-perf";
 
@@ -13,25 +13,52 @@ export default function Experience() {
       <ambientLight intensity={1.5} />
 
       <Physics debug>
-        <RigidBody>
-          <mesh castShadow position={[-2, 2, 0]}>
+        <RigidBody colliders={"ball"} position={[-2, 2, 0]}>
+          <mesh castShadow>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
           </mesh>
+          <Html transform>
+            <h1 className="text-2xl">ball</h1>
+          </Html>
         </RigidBody>
 
-        <RigidBody>
-          <mesh castShadow position={[2, 2, 0]}>
+        <RigidBody position={[2, 2, 0]}>
+          <mesh castShadow>
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
           </mesh>
+          <Html transform>
+            <h1 className="text-2xl">default</h1>
+          </Html>
         </RigidBody>
 
-        <RigidBody position={[0, 5, 0]}>
+        <RigidBody
+          position={[3, 5, 0]}
+          rotation={[Math.PI / 8, 0, 0]}
+          colliders={"hull"}
+        >
           <mesh castShadow>
             <torusGeometry></torusGeometry>
             <meshStandardMaterial color={"red"}></meshStandardMaterial>
           </mesh>
+          <Html transform>
+            <h1 className="text-2xl">hull</h1>
+          </Html>
+        </RigidBody>
+
+        <RigidBody
+          position={[0, 5, 0]}
+          rotation={[Math.PI / 8, 0, 0]}
+          colliders={"trimesh"}
+        >
+          <mesh castShadow>
+            <torusGeometry></torusGeometry>
+            <meshStandardMaterial color={"red"}></meshStandardMaterial>
+          </mesh>
+          <Html transform>
+            <h1 className="text-2xl">trimesh</h1>
+          </Html>
         </RigidBody>
 
         <RigidBody type="fixed">
